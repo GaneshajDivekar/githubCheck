1d8df791981c105d15160c6b96ba954bf787246a
ganeshajdivekar <ganeshajdivekar@gmail.com>
2025-02-22 14:31:02 +0530

    Initial commit: Added script for GitHub activity generation

--- contribute.py
+++ contribute.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python
 import argparse
 import os
-from datetime import datetime
-from datetime import timedelta
+from datetime import datetime, timedelta
 from random import randint
 from subprocess import Popen
 import sys
-
 
 def main(def_args=sys.argv[1:]):
     args = arguments(def_args)
     curr_date = datetime.now()
     directory = 'repository-' + curr_date.strftime('%Y-%m-%d-%H-%M-%S')
     repository = args.repository
-    user_name = args.user_name
-    user_email = args.user_email
+    user_name = "ganeshajdivekar"
+    user_email = "ganeshajdivekar@gmail.com"
+
     if repository is not None:
         start = repository.rfind('/') + 1
         end = repository.rfind('.')
@@ -31,19 +30,13 @@ def main(def_args=sys.argv[1:]):
     os.chdir(directory)
     run(['git', 'init', '-b', 'main'])
 
-    if user_name is not None:
-        run(['git', 'config', 'user.name', user_name])
-
-    if user_email is not None:
-        run(['git', 'config', 'user.email', user_email])
+    run(['git', 'config', 'user.name', user_name])
+    run(['git', 'config', 'user.email', user_email])
 
     start_date = curr_date.replace(hour=20, minute=0) - timedelta(days_before)
-    for day in (start_date + timedelta(n) for n
-                in range(days_before + days_after)):
-        if (not no_weekends or day.weekday() < 5) \
-                and randint(0, 100) < frequency:
-            for commit_time in (day + timedelta(minutes=m)
-                                for m in range(contributions_per_day(args))):
+    for day in (start_date + timedelta(n) for n in range(days_before + days_after)):
+        if (not no_weekends or day.weekday() < 5) and randint(0, 100) < frequency:
+            for commit_time in (day + timedelta(minutes=m) for m in range(contributions_per_day(args))):
                 contribute(commit_time)
 
     if repository is not None:
@@ -51,25 +44,19 @@ def main(def_args=sys.argv[1:]):
         run(['git', 'branch', '-M', 'main'])
         run(['git', 'push', '-u', 'origin', 'main'])
 
-    print('\nRepository generation ' +
-          '\x1b[6;30;42mcompleted successfully\x1b[0m!')
-
+    print('\nRepository generation \x1b[6;30;42mcompleted successfully\x1b[0m!')
 
 def contribute(date):
     with open(os.path.join(os.getcwd(), 'README.md'), 'a') as file:
         file.write(message(date) + '\n\n')
     run(['git', 'add', '.'])
-    run(['git', 'commit', '-m', '"%s"' % message(date),
-         '--date', date.strftime('"%Y-%m-%d %H:%M:%S"')])
-
+    run(['git', 'commit', '-m', '"%s"' % message(date), '--date', date.strftime('"%Y-%m-%d %H:%M:%S"')])
 
 def run(commands):
     Popen(commands).wait()
 
-
 def message(date):
     return date.strftime('Contribution: %Y-%m-%d %H:%M')
-
 
 def contributions_per_day(args):
     max_c = args.max_commits
@@ -79,50 +66,21 @@ def contributions_per_day(args):
         max_c = 1
     return randint(1, max_c)
 
-
 def arguments(argsval):
     parser = argparse.ArgumentParser()
-    parser.add_argument('-nw', '--no_weekends',
-                        required=False, action='store_true', default=False,
-                        help="""do not commit on weekends""")
-    parser.add_argument('-mc', '--max_commits', type=int, default=10,
-                        required=False, help="""Defines the maximum amount of
-                        commits a day the script can make. Accepts a number
-                        from 1 to 20. If N is specified the script commits
-                        from 1 to N times a day. The exact number of commits
-                        is defined randomly for each day. The default value
-                        is 10.""")
-    parser.add_argument('-fr', '--frequency', type=int, default=80,
-                        required=False, help="""Percentage of days when the
-                        script performs commits. If N is specified, the script
-                        will commit N%% of days in a year. The default value
-                        is 80.""")
+    parser.add_argument('-nw', '--no_weekends', required=False, action='store_true', default=False,
+                        help="do not commit on weekends")
+    parser.add_argument('-mc', '--max_commits', type=int, default=10, required=False,
+                        help="Defines the max commits per day (1 to 20). Default is 10.")
+    parser.add_argument('-fr', '--frequency', type=int, default=80, required=False,
+                        help="Percentage of days to commit (default is 80%).")
     parser.add_argument('-r', '--repository', type=str, required=False,
-                        help="""A link on an empty non-initialized remote git
-                        repository. If specified, the script pushes the changes
-                        to the repository. The link is accepted in SSH or HTTPS
-                        format. For example: git@github.com:user/repo.git or
-                        https://github.com/user/repo.git""")
-    parser.add_argument('-un', '--user_name', type=str, required=False,
-                        help="""Overrides user.name git config.
-                        If not specified, the global config is used.""")
-    parser.add_argument('-ue', '--user_email', type=str, required=False,
-                        help="""Overrides user.email git config.
-                        If not specified, the global config is used.""")
-    parser.add_argument('-db', '--days_before', type=int, default=365,
-                        required=False, help="""Specifies the number of days
-                        before the current date when the script will start
-                        adding commits. For example: if it is set to 30 the
-                        first commit date will be the current date minus 30
-                        days.""")
-    parser.add_argument('-da', '--days_after', type=int, default=0,
-                        required=False, help="""Specifies the number of days
-                        after the current date until which the script will be
-                        adding commits. For example: if it is set to 30 the
-                        last commit will be on a future date which is the
-                        current date plus 30 days.""")
+                        help="Link to an empty non-initialized remote Git repository.")
+    parser.add_argument('-db', '--days_before', type=int, default=365, required=False,
+                        help="Number of past days to start adding commits. Default is 365.")
+    parser.add_argument('-da', '--days_after', type=int, default=0, required=False,
+                        help="Number of future days to continue adding commits. Default is 0.")
     return parser.parse_args(argsval)
-
 
 if __name__ == "__main__":
     main()
